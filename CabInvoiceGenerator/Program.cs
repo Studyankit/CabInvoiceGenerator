2be1abeac29ca426ccdc035dbe69436431371450
StudyAnkit <ankitghosh2018@gmail.com>
2022-03-20 09:33:44 +0530

    [Ankit]- Calculate cab fare

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿using CabInvoiceGenerator;
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+double fare = invoiceGenerator.CalculateFare(5.0, 5);
+Console.WriteLine($"Fare : {fare}");
+Console.ReadLine();
